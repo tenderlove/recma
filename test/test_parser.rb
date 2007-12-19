5f3098d51bdbc3204db6eeafcf47856fcf629915
aaronp <aaronp@bf63cea8-67a3-4864-ab90-c58634fce689>
2007-12-19 06:46:51 +0000

    testing assignment of null literal
    git-svn-id: svn://rubyforge.org/var/svn/rkelly/trunk@35 bf63cea8-67a3-4864-ab90-c58634fce689

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -19,6 +19,13 @@ class ParserTest < Test::Unit::TestCase
     )
   end
 
+  def test_variable_declaration_nil_init
+    assert_sexp(
+      [:var, [[:var_decl, :foo, [:assign, [:nil]]]]],
+      @parser.parse('var foo = null;').to_sexp
+    )
+  end
+
   def test_variable_statement_no_semi
     assert_sexp(
       [:var, [[:var_decl, :foo, [:assign, [:lit, "10"]]]]],
