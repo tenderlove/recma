9057e293e7a6b2a852f7465eb2af4ca22b061a83
Aaron Patterson <aaron.patterson@gmail.com>
2008-07-27 15:32:13 -0700

    making the tests pass

--- lib/rkelly/nodes.rb
+++ lib/rkelly/nodes.rb
@@ -1,4 +1,5 @@
 require 'rkelly/nodes/node'
+require 'rkelly/nodes/function_expr_node'
 Dir[File.join(File.dirname(__FILE__), "nodes/*_node.rb")].each do |file|
   require file[/rkelly\/nodes\/.*/]
 end
